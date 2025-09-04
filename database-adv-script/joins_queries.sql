b9978742ecad7d77bf70fd3deaf01cd118681f76
Audrey-Okumu <okumuaudrey71@gmail.com>
2025-09-04 18:27:18 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -11,7 +11,8 @@ SELECT
     u.email
 FROM bookings b
 INNER JOIN users u
-ON b.user_id = u.user_id;
+ON b.user_id = u.user_id
+ORDER BY b.booking_id;
 
 -- Retrieve all properties and their reviews (even if a property has no review)
 SELECT 
@@ -22,7 +23,8 @@ SELECT
     r.comment
 FROM properties p
 LEFT JOIN reviews r
-ON p.property_id = r.property_id;
+ON p.property_id = r.property_id
+ORDER BY p.property_id, r.review_id;
 
 -- Retrieve all users and all bookings, even if no matching booking or user
 SELECT 
@@ -36,5 +38,5 @@ SELECT
     b.total_price
 FROM users u
 FULL OUTER JOIN bookings b
-ON u.user_id = b.user_id;
-
+ON u.user_id = b.user_id
+ORDER BY u.user_id, b.booking_id;
